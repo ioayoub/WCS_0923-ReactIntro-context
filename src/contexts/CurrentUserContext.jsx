c20a4dcd300ff5c88e27686be05194ec603f44e4
IDRISSI O. Ayoub <idrissio.ayoub@gmail.com>
2023-11-17 18:22:37 +0100

    add: useMemo feature to show you an other way to handle contexts

--- src/contexts/CurrentUserContext.jsx
+++ src/contexts/CurrentUserContext.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { createContext, useContext } from "react";
+import { useState, createContext, useContext, useMemo } from "react";
+import {} from "react";
 import PropTypes from "prop-types";
 
 const UserContext = createContext({
@@ -10,26 +10,29 @@ const UserContext = createContext({
 export const UserProvider = ({ children }) => {
   const [name, setName] = useState(localStorage.getItem("name") || "");
 
-  const handleNameChange = (e) => {
-    e.preventDefault();
-    let inputValue = e.target[0].value;
+  const userValues = useMemo(
+    () => ({
+      name,
+      handleNameChange: (e) => {
+        e.preventDefault();
+        let inputValue = e.target[0].value;
 
-    if (inputValue.trim() === "") return alert("Please enter a name");
+        if (inputValue.trim() === "") return alert("Please enter a name");
 
-    setName(inputValue);
-    // localStorage.setItem("name", inputValue);
-    e.target[0].value = "";
-  };
-
-  const resetName = () => {
-    setName("");
-    localStorage.removeItem("name");
-  };
+        setName(inputValue);
+        // localStorage.setItem("name", inputValue);
+        e.target[0].value = "";
+      },
+      resetName: () => {
+        setName("");
+        localStorage.removeItem("name");
+      },
+    }),
+    [name]
+  );
 
   return (
-    <UserContext.Provider value={{ name, handleNameChange, resetName }}>
-      {children}
-    </UserContext.Provider>
+    <UserContext.Provider value={userValues}>{children}</UserContext.Provider>
   );
 };
 
